e039d7e00c41a8c22a2039894ef93fab18f55c61
DevMountain <devmountain@DevMountains-MacBook-Pro.local>
2019-04-26 16:49:49 -0500

    blank react app

--- src/App.js
+++ src/App.js
@@ -3,28 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  state = {
-    dropDownMenuStatus: ''
-  }
 
 
   render() {
     return (
       <div className="App">
-        <menu className={this.state.dropDownMenuStatus}>
-          
-        </menu>
-        <nav>
-          <h1>Website</h1>
-          <button
-          onClick={() => this.setState({
-            dropDownMenuStatus: 
-            this.state.dropDownMenuStatus === 'drop-down-menu-open' ?
-            'drop-down-menu-closed' :
-            'drop-down-menu-open'
-          })}
-          >Do Stuff</button>
-        </nav>
       </div>
     );
   }
